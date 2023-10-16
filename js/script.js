c102c64113ba710b3aedb64b62ebf17741a54d0a
FlorenciaNieva <florencianieva930@gmail.com>
2023-10-16 11:29:56 -0300

    Incorporo divs con locación, categoria y antiguedad en las tarjetas

--- js/script.js
+++ js/script.js
@@ -2,13 +2,18 @@ const $ = (selector) => document.querySelector(selector);
 const $$ = (selector) => document.querySelectorAll(selector);
 
 const cardJob = (data) => {
-    for (let {name, image, description, languajes, location} of data) {
+    for (let {name, image, description, location, seniority, category} of data) {
     $('#container-cards').innerHTML += `<div class="card col-4 m-1" style="width: 16rem;">
         <img src="${image}" class="card-img-top mt-2" alt="factory">
         <div class="card-body">
             <h5 class="card-title">${name}</h5>
             <p class="card-text">${description}</p>
-            <a href="#" class="btn btn-primary">See details</a>
+            <div class="d-grid gap-2 d-md-flex justify-content-center mb-2">
+                <div class="btn-card btn btn-primary">${location}</div>
+                <div class="btn-card btn btn-primary">${seniority}</div>
+                <div class="btn-card btn btn-primary">${category}</div>
+            </div>
+            <a href="#" class="btn btn-primary d-flex justify-content-center">See details</a>
         </div>
     </div>` }
 }
